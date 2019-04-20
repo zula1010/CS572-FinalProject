cd84ec6dd5ac85ab49832982294f816501a4b792
HaiHNguyen <haihong.nguyen.vn@gmail.com>
2019-04-20 13:14:29 -0500

    Added routing for Book and add new packages

--- app_server/app.js
+++ app_server/app.js
@@ -10,6 +10,9 @@ const loginRouter = require("./routes/login");
 const authChek = require("./authCheck");
 var app = express();
 
+//Book Management
+const bookRouter = require('./routes/bookroutes');
+
 // view engine setup
 
 
@@ -22,11 +25,15 @@ app.use(cors());
 app.use("api/admin",authChek("admin"));
 app.use("api/lib",authChek("lib"));
 loginRouter.use("/login",loginRouter);
+
+//Books Management
+app.use('/books', bookRouter);
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(3000);
+    app.listen(3000, ()=>console.log("Server is running on port 3000"));
   }).catch(err => console.log(err));
 
 
