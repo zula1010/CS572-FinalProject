548f5403e433ebcb34f6df8e68c85a0c952bb57c
Munkhzul Khurelbaatar <julia@Munkhzuls-MacBook-Pro.local>
2019-04-23 11:17:30 -0500

    checkout UI created

--- frontend/src/app/checkout/checkout.component.ts
+++ frontend/src/app/checkout/checkout.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { ReaderService } from '../reader.service';
+import { Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material';
+import { startWith, map } from 'rxjs/operators';
+import { DataSource } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +12,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
-
+  checkoutForm: FormGroup;
+  message;
+  reader_id;
+  book_id;
   ngOnInit() {
+
+
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.checkoutForm = formBuilder.group({
+      'reader': [''],
+      'book': ['']
+    })
+  }
+  onCheckout() {
+    this.reader_id = this.checkoutForm.value.reader;
+    this.book_id = this.checkoutForm.value.book;
+    console.log(this.reader_id + " ---" + this.book_id);
   }
 
 }
